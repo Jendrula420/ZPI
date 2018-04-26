5cb3f2e418d6f96c4028c0b94907c0c2dd6e9f48
Andrzej <Andrzej@PWr>
2018-04-26 08:16:40 +0200

    /10

--- app/src/main/java/com/sourcey/materiallogindemo/PresenceActivity.java
+++ app/src/main/java/com/sourcey/materiallogindemo/PresenceActivity.java
@@ -1,8 +1,19 @@
 package com.sourcey.materiallogindemo;
 
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.FormatException;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.Ndef;
+import android.os.Parcelable;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -11,6 +22,8 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -20,6 +33,8 @@ import com.google.firebase.database.ValueEventListener;
 
 import org.w3c.dom.Text;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -37,9 +52,10 @@ public class PresenceActivity extends AppCompatActivity {
     Button butOdswiez;
     Button butWyloguj;
     Button butUstal;
+    Button butObecnosc;
     Spinner spinnerProwadzacy;
     ArrayAdapter<String> spinnerAdapter;
-    ArrayList<String[]> daneKursow; //[0]idProw, [1]idGrupy, [2]kodKursu, [3]nazwaKursu, [4]dzienTyg, [5]godzRoz, [6]godzZak
+
 
     ArrayList<String> elementySpinner;
     ArrayList<String> prowadzacyList;
@@ -48,9 +64,20 @@ public class PresenceActivity extends AppCompatActivity {
     String wybranyProw;
     String wybranyKurs;
     String idGrupy;
+    String kod;
 
     FirebaseAuth firebaseAuth;
     DatabaseReference database;
+
+    //NFC
+    public final String ERROR_DETECTED = "Nie wykryto taga NFC"; //zamienic na R.strings
+    public final String WRITE_SUCCESS = "Pomyślnie zapisano na tagu";
+    public final String WRITE_ERROR = "Zbliż tag jeszcze raz";
+    NfcAdapter nfcAdapter;
+    PendingIntent pendingIntent;
+    IntentFilter writeTagFilters[];
+    boolean writeMode;
+    Tag myTag;
 
 
 
@@ -65,9 +92,9 @@ public class PresenceActivity extends AppCompatActivity {
         butOdswiez = findViewById(R.id.but_odswiez2);
         butWyloguj = findViewById(R.id.but_wyloguj2);
         butUstal = findViewById(R.id.but_ustal);
+        butObecnosc = findViewById(R.id.but_obecnosc);
         spinnerProwadzacy = findViewById(R.id.spinner_prowadzacy);
 
-        daneKursow = new ArrayList<>();
         elementySpinner = new ArrayList<>();
         prowadzacyList = new ArrayList<>();
         firebaseAuth = FirebaseAuth.getInstance();
@@ -75,7 +102,21 @@ public class PresenceActivity extends AppCompatActivity {
 
         ustalIdStudent();
 
+        butObecnosc.setEnabled(false);
 
+
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        if (nfcAdapter == null) {
+            Toast.makeText(this, "Twój telefon nie obsługuje NFC", Toast.LENGTH_LONG).show();
+            finish();
+        }
+
+        readFromIntent(getIntent());
+
+        pendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+        IntentFilter tagDetected = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED);
+        tagDetected.addCategory(Intent.CATEGORY_DEFAULT);
+        writeTagFilters = new IntentFilter[] { tagDetected };
 
 
         butOdswiez.setOnClickListener(new View.OnClickListener() {
@@ -103,8 +144,21 @@ public class PresenceActivity extends AppCompatActivity {
                 ustalKurs();
                 if(wybranyKurs == null)
                     textKurs.setText("Nie ustalono kursu");
-                else
+                else {
                     textKurs.setText(wybranyKurs);
+                    butObecnosc.setEnabled(true);
+                }
+
+            }
+        });
+
+        butObecnosc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(PresenceActivity.this, "Zbliż tag", Toast.LENGTH_LONG).show();
+
+
 
             }
         });
@@ -231,6 +285,108 @@ public class PresenceActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    //Zapisywanie na NFC
+
+
+    private void readFromIntent(Intent intent) {
+        String action = intent.getAction();
+        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(action)
+                || NfcAdapter.ACTION_TECH_DISCOVERED.equals(action)
+                || NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
+            Parcelable[] rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+            NdefMessage[] msgs = null;
+            if (rawMsgs != null) {
+                msgs = new NdefMessage[rawMsgs.length];
+                for (int i = 0; i < rawMsgs.length; i++) {
+                    msgs[i] = (NdefMessage) rawMsgs[i];
+                }
+            }
+            buildTagViews(msgs);
+        }
+    }
+
+    private void buildTagViews(NdefMessage[] msgs) {
+        if (msgs == null || msgs.length == 0) return;
+
+        String text = "";
+        byte[] payload = msgs[0].getRecords()[0].getPayload();
+        String textEncoding = ((payload[0] & 128) == 0) ? "UTF-8" : "UTF-16"; // Get the Text Encoding
+        int languageCodeLength = payload[0] & 0063;
+
+        try {
+            text = new String(payload, languageCodeLength + 1, payload.length - languageCodeLength - 1, textEncoding);
+        } catch (UnsupportedEncodingException e) {
+            Log.e("UnsupportedEncoding", e.toString());
+        }
+
+        kod = text;
+        if(butObecnosc.isEnabled()){
+            zapiszObecnosc();
+
+        }
+    }
+
+
+
+    private void zapiszObecnosc(){
+        database.child("Kody").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String kodObecnosci = dataSnapshot.child(idGrupy.toString()).child("kod").getValue().toString().trim();
+                if(kodObecnosci.equals(kod)){
+                    database.child("Obecnosci").child(idGrupy.toString()).child(nrAlbumu.toString()).setValue("Obecny")
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if(task.isSuccessful())
+                                        Toast.makeText(PresenceActivity.this, "Twoja obecność została odnotowana", Toast.LENGTH_LONG).show();
+                                    else
+                                        Toast.makeText(PresenceActivity.this, "Wystąpił błąd", Toast.LENGTH_LONG).show();
+                                }
+                            });
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        setIntent(intent);
+        readFromIntent(intent);
+        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(intent.getAction())){
+            myTag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+        }
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        WriteModeOff();
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        WriteModeOn();
+    }
+
+
+    private void WriteModeOn(){
+        writeMode = true;
+        nfcAdapter.enableForegroundDispatch(this, pendingIntent, writeTagFilters, null);
+    }
+
+    private void WriteModeOff(){
+        writeMode = false;
+        nfcAdapter.disableForegroundDispatch(this);
     }
 
 
